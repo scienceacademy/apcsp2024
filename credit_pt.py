a81d40b62682905000ae46c4b037c02c8554e630
Chris Bradfield <cb@scribe.net>
2024-11-18 07:35:42 -0800

    Create credit_pt.py

---
+++ credit_pt.py
@@ -0,0 +1,30 @@
+# A version of credit.py that fulfills the Create Task requirements.
+
+def luhn(number):
+    sum = 0
+    for n in card[-2::-2]:
+        sum += n * 2 % 10 + n * 2 // 10
+    for n in card[-1::-2]:
+        sum += n
+    return sum % 10
+
+
+# Get card number
+card = ""
+while not card.isdigit() or int(card) < 0:
+    card = input("Number: ")
+length = len(card)
+
+# list comprehensions are not recommended for your loop requirement
+# if (sum([int(n) * 2 % 10+int(n) * 2//10 for n in card[-2::-2]]) + sum([int(n) for n in card[-1::-2]])) % 10:
+#     print("INVALID")
+if !luhn(card):
+    print("INVALID")
+elif length == 15 and card[0:2] == "34" or card[0:2] == "37":
+    print("AMEX")
+elif length == 16 and int(card[0:2]) in range(51,56):
+    print("MASTERCARD")
+elif length == 13 or length == 16 and card[0] == "4":
+    print("VISA")
+else:
+    print("INVALID")
